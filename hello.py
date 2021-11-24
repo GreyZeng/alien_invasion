8691c900bf5e7ebf92dffa9a03d4e8c8337a3d88
GreyZeng <410486047@qq.com>
2021-11-24 22:48:09 +0800

    update

--- hello.py
+++ hello.py
@@ -1,2 +1,11 @@
-
 print("Hello world")
+
+print("Hello", "Welcome to", "China")
+
+print("100 + 200 = ", 100 + 200)
+
+name = input()
+print(name)
+
+name = input("please input your name: ")
+print(name)

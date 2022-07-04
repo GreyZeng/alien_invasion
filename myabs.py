d0b6e9b3de2bcc8f05b779d2ae7123f708f59938
GreyZeng <410486047@qq.com>
2022-07-04 22:29:39 +0800

    update

--- myabs.py
+++ myabs.py
@@ -17,3 +17,31 @@ def move(x, y, step, angle=0):
 r = move(100, 100, 60, math.pi / 6)
 
 print(r)
+
+
+def power(x, n=2):
+    s = 1
+    while n > 0:
+        n = n - 1
+        s = s * x
+    return s
+
+print(power(3))
+print(power(2,4))
+
+# 注意两个方法的区别
+def add_end1(L=[]):
+    L.append('END')
+    return L
+print(add_end1())
+print(add_end1())
+print(add_end1())
+def add_end2(L=None):
+    if L is None:
+        L = []
+    L.append('END')
+    return L
+
+
+print(add_end2([1,2,3]))
+print(add_end2([1,2,3]))

58e9f8b7742296160c4949a37d40bdc8af2f5a7a
GreyZeng <410486047@qq.com>
2023-10-09 11:39:36 +0800

    update code

--- hi.py
+++ hi.py
@@ -1,2 +1,6 @@
-import sys
-print(sys.path)
+message = 'I told my friend, "Python is my favorite language!"'
+print(message)
+message = "I told my friend, 'Python is my favorite language!'"
+print(message)
+message = "One of Python's strength"
+print(message)

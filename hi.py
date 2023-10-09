a97e7009e234c4ec0a71c172530cae8c349fb618
GreyZeng <410486047@qq.com>
2023-10-09 12:32:17 +0800

    update code

--- hi.py
+++ hi.py
@@ -4,3 +4,11 @@ message = "I told my friend, 'Python is my favorite language!'"
 print(message)
 message = "One of Python's strength"
 print(message)
+message = 'a da lovelace'
+print(message.title())
+first_name = "ada"
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}"
+print(full_name)
+# 打印 Ada Lovelace
+print(f'Hello, {full_name.title()}')

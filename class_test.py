978f8917e134eec964c4a58b3ad9545dda0fe5b8
GreyZeng <410486047@qq.com>
2023-10-20 23:44:53 +0800

    update code

--- class_test.py
+++ class_test.py
@@ -1,5 +1,5 @@
 class Dog:
-    def __init__(self,name,age):
+    def __init__(self, name, age):
         self.name = name
         self.age = age
 
@@ -10,8 +10,24 @@ class Dog:
         print(f"{self.name} rolled over")
 
 
-mydog = Dog("zss",12)
+class ChildDog(Dog):
+
+    def __init__(self, name, age):
+        super().__init__("Child" + name, age)
+        self.new_fac = "abc"
+
+    def child_method(self):
+        print(f"{self.name} age is {self.age}")
+
+    def roll_over(self):
+        print(f"{self.name} age is {self.age} new fac is {self.new_fac}")
+
+
+mydog = Dog("zss", 12)
 mydog.sit()
 mydog.roll_over()
 mydog.name = "list"
 mydog.roll_over()
+my_child_dog = ChildDog("xxx", 11)
+my_child_dog.child_method()
+my_child_dog.roll_over()

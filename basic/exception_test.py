73d64dfc15f7fc218924c8c289ffff3d1df14404
GreyZeng <410486047@qq.com>
2023-10-21 20:23:52 +0800

    update code

--- basic/exception_test.py
+++
@@ -1,5 +0,0 @@
-try:
-    print(5/0)
-except ZeroDivisionError:
-    # 如果不处理异常，则可以写 pass
-    print("Zero div error")
--- basic/test.py
+++
@@ -1,5 +0,0 @@
-cars = ['a','c','b']
-cars.sort(reverse=True)
-print(cars)
-cars.reverse()
-print(cars)

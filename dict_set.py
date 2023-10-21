ce882ef271468e501629b9d7e254b802f0047f0b
GreyZeng <410486047@qq.com>
2023-10-21 12:55:21 +0800

    update code

--- dict_set.py
+++ dict_set.py
@@ -1,23 +1,19 @@
-d={'A':33,'B':44,'C':445}
-#print(d['A'])
+d = {'A': 456, 'B': 44, 'C': 445}
+# print(d['A'])
 
-
-d['A']=456
 
 # print(d['A'])
 
 if 'D' in d:
     print(d['D'])
 elif 'A' in d:
-    print(d.get('D',-1))
+    print(d.get('D', -1))
 else:
     print(d['C'])
-
 
 s = set([1, 1, 2, 2, 3, 3])
 
 print(s)
-
 
 s1 = set([1, 2, 3])
 s2 = set([2, 3, 4])

66a72da47ce556bb1125556a900f67814f5eab5b
GreyZeng <410486047@qq.com>
2023-10-22 09:15:10 +0800

    update color

--- settings.py
+++ settings.py
@@ -3,4 +3,4 @@ class Settings:
     def __init__(self):
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (230, 233, 200)
+        self.bg_color = (135, 206, 250)

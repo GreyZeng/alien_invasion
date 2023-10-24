43dc37f4dd856802a50fc567b675b0bae50b7960
GreyZeng <410486047@qq.com>
2023-10-24 13:26:56 +0800

    update code

--- alien_invasion.py
+++ alien_invasion.py
@@ -31,20 +31,25 @@ class AlienInvasion:
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                self.check_keydown_events(event)
+                self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
-                self.check_keyup_events(event)
+                self._check_keyup_events(event)
 
-    def check_keydown_events(self, event):
+    def _check_keydown_events(self, event):
         if event.key == pygame.K_RIGHT:
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
-    def check_keyup_events(self, event):
+        elif event.key == pygame.K_q:
+            # 输入 Q 键 也可以退出
+            sys.exit()
+
+    def _check_keyup_events(self, event):
         if event.key == pygame.K_RIGHT:
             self.ship.moving_right = False
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
+
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
